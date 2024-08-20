1c2cdd5a4132fa34f30ddc0106c8e2ce4eb09bd3
fdhhhdjd <nguyentientai10@gmail.com>
2024-08-20 18:03:11 +0700

    feat: finish model face

--- express-backend/src/app/services/identifyFaceServices.js
+++ express-backend/src/app/services/identifyFaceServices.js
@@ -1,50 +1,49 @@
 "use strict";
-const bcrypt = require("bcryptjs");
+
 const faceapi = require("@vladmandic/face-api");
+const { v4: uuidv4 } = require("uuid");
 require("@tensorflow/tfjs-node");
 
 const { BadRequestResponse } = require("../../cors/error.response");
 const pgInit = require("../../inits/pgInit");
-const TokenHelpers = require("../../helpers/tokenHelpers");
+const SessionHelpers = require("../../helpers/sessionHelpers");
+const CookieHelpers = require("../../helpers/cookieHelpers");
+const cookieConstants = require("../../constants/cookieConstants");
+const db = require("../../data/dummy/db");
 
 class IdentifyFaceServices {
   static async register(req, __) {
-    const { username, password, faceData } = req.body;
+    const { email, faceData } = req.body;
 
-    if ((username, faceData)) {
+    if (!email && !faceData) {
       throw new BadRequestResponse();
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pgInit
       .getPool()
       .query(
-        "INSERT INTO users (username, password, face_data) VALUES ($1, $2, $3) RETURNING *",
-        [username, hashedPassword, faceData]
+        "INSERT INTO users (email, face_data) VALUES ($1, $2) RETURNING *",
+        [email, faceData]
       );
 
     if (!result.rows[0]) {
       throw new BadRequestResponse();
     }
 
-    const token = TokenHelpers.generateToken(result.rows[0]);
-
-    if (!token) {
-      throw new BadRequestResponse();
-    }
-
-    return token;
+    return result.rows[0] || {};
   }
 
   static async login(req, res) {
-    const { username, password, faceData } = req.body;
+    const { email, faceData } = req.body;
 
-    if ((username, faceData)) throw new BadRequestResponse();
+    if (!email && !faceData) throw new BadRequestResponse();
 
     const result = await pgInit
       .getPool()
-      .query("SELECT * FROM users WHERE username = $1", [username]);
+      .query("SELECT * FROM users WHERE email = $1", [
+        "nguyentientai9@gmail.com",
+      ]);
+
     if (result.rows.length === 0) throw new BadRequestResponse();
 
     const user = result.rows[0];
@@ -55,6 +54,36 @@ class IdentifyFaceServices {
     );
     const queryFaceData = new Float32Array(Object.values(JSON.parse(faceData)));
     const distance = faceapi.euclideanDistance(storedFaceData, queryFaceData);
+
+    if (distance > 0.6)
+      throw new BadRequestResponse({
+        message: "Face not recognized",
+      });
+
+    const tempSessionId = uuidv4();
+
+    if (!tempSessionId) {
+      throw new BadRequestResponse();
+    }
+
+    const userSession = db.users.find(
+      (user) => user.email === result.rows[0].email
+    );
+    console.log(userSession);
+
+    if (!userSession) {
+      throw new BadRequestResponse();
+    }
+
+    SessionHelpers.setUser(tempSessionId, userSession);
+
+    CookieHelpers.saveCookie(res, cookieConstants.LOGIN, tempSessionId);
+
+    return {
+      id: user.id,
+      email: user.email,
+      session: tempSessionId,
+    };
   }
 }
 
