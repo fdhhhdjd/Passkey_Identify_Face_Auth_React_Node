ec36f590c4a6481aa77da01338524067f0e7a75b
fdhhhdjd <nguyentientai10@gmail.com>
2024-08-21 11:42:47 +0700

    fix: cors

--- express-backend/src/helpers/cookieHelpers.js
+++ express-backend/src/helpers/cookieHelpers.js
@@ -19,23 +19,13 @@ class CookieHelpers {
   }
 
   static saveCookie(res, key, value, time = timeConstants._1_DAY * 30) {
-    const headerValue =
-      appConstants.NODE_APP[0] === process.env.NODE_ENV
-        ? "localhost"
-        : res.headers["x-forwarded-host"];
-
-    const headerArray = Array.isArray(headerValue)
-      ? headerValue
-      : [headerValue];
-    const headerParts = headerArray[0]?.split(":");
-
     const option = {
       httpOnly: true,
-      secure: false, // true for production with HTTPS
-      sameSite: "None",
-      maxAge: 1000 * 60 * 60 * 24,
-      // domain: headerParts[0],
-      // partitioned: true,
+      sameSite:
+        process.env.NODE_ENV === appConstants.NODE_APP[0] ? false : true,
+      secure: process.env.NODE_ENV === appConstants.NODE_APP[0] ? false : true,
+      maxAge: time,
+      partitioned: true,
     };
 
     res.cookie(key, value, option);
